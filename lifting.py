093b9ab764855576c8e60f77adcdc45135613d1a
Frederic Marazzato <frederic.marazzato@unlv.edu>
2026-05-05 11:29:12 -0700

    Adding the file with the definition of the LiftJump class and some tests.

---
+++ lifting.py
@@ -0,0 +1,34 @@
+from firedrake import *
+from firedrake.external_operators import AbstractExternalOperator, assemble_method
+
+
+class LiftJump(AbstractExternalOperator):
+
+    def __init__(self, *operands, function_space, **kwargs):
+        AbstractExternalOperator.__init__(self, *operands, function_space=function_space, **kwargs)
+    
+    def assemble_matrix(self, w):
+        Sigma = self.function_space()
+        mesh = Sigma.mesh()
+        tau = TestFunction(Sigma)
+        n = FacetNormal(mesh)
+        OG_space = w.function_space()
+        u = TrialFunction(OG_space)
+        return inner(jump(u), dot(avg(tau), n("+"))) * dS(mesh) + inner(u, dot(tau, n)) * ds(mesh)
+
+    @assemble_method(0, (0,))
+    def assemble_operator(self, *args, **kwargs):
+        """Assemble LiftJump(u)."""
+        (u,) = self.ufl_operands
+        a = self.assemble_matrix(u)
+        cofunc = assemble(action(a, u))
+        return cofunc.riesz_representation() #function in primal space
+
+    @assemble_method((1,), (0, 1))
+    def assemble_Jacobian(self, *args, **kwargs):
+        """Since the operator is linear this is similar to just the assembly."""
+        (u,) = self.ufl_operands
+        a = self.assemble_matrix(u)
+        A = assemble(a)
+        return A
+
